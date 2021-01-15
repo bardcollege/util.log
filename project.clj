58c6f59d8439d6a511d44fb4e272ae20a420f0ee
Hayden Sartoris <hsartoris@bard.edu>
2021-01-15 14:24:13 -0500

    configured deploy to clojars

--- project.clj
+++ project.clj
@@ -4,11 +4,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repl-options {:init-ns idm.log}
-  :deploy-repositories [["github" {:url "https://maven.pkg.github.com/bardcollege/idm.log"
-                                   ; causes leiningen to look for LEIN_USERNAME and LEIN_PASSWORD
-                                   :username :env
-                                   :password :env
-                                   :sign-releases false}]]
+  :deploy-repositories {"github" {:url "https://maven.pkg.github.com/bardcollege/idm.log"
+                                  ; causes leiningen to look for LEIN_USERNAME and LEIN_PASSWORD
+                                  :username :env
+                                  :password :env
+                                  :sign-releases false}
+                        "clojars" {:url "https://repo.clojars.org"
+                                   :username :env/clojars_user
+                                   :password :env/clojars_password
+                                   :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.apache.logging.log4j/log4j-api "2.13.3"]
