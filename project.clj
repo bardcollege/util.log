5ccb28a1c435a22aa4d6846964feaa809ebb2b0a
Hayden Sartoris <hsartoris@bard.edu>
2021-01-15 12:20:47 -0500

    add lein release options

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/bardcollege/idm.log"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repl-options {:init-ns idm.log}
   :deploy-repositories [["github" {:url "https://maven.pkg.github.com/bardcollege/idm.log"
                                    ; causes leiningen to look for LEIN_USERNAME and LEIN_PASSWORD
                                    :username :env
@@ -15,4 +16,11 @@
                  [org.apache.logging.log4j/log4j-slf4j-impl "2.13.3"]
                  [org.slf4j/slf4j-api "1.7.30"]
                  [com.fasterxml.jackson.dataformat/jackson-dataformat-yaml "2.11.1"]]
-  :repl-options {:init-ns idm.log})
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" ":release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
+
