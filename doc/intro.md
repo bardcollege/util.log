24c07e0e26be4ac067dc733b4c9022429e2f9cda
Hayden Sartoris <hsartoris@bard.edu>
2021-01-15 10:18:40 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to idm.log
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/idm/log_test.clj
@@ -0,0 +1,7 @@
+(ns idm.log-test
+  (:require [clojure.test :refer :all]
+            [idm.log :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
